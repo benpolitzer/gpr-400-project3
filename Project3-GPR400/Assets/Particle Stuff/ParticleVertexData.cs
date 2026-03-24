c8bf0e1fce727d33d366697d6dc361cc5df73c96
victor <victor.diab@mymail.champlain.edu>
2026-03-24 00:57:15 -0400

    working properly, problem with other objects was their mesh topology wasn't point, in scripting we set that so now it works.

---
+++ Project3-GPR400/Assets/Particle Stuff/ParticleVertexData.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParticleVertexData : MonoBehaviour
+{
+    MeshFilter mf;
+
+    private void Awake()
+    {
+        mf = GetComponent<MeshFilter>();
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0)
+        };
+
+        mesh.vertices = vertices;
+
+        int[] indices = new int[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+            indices[i] = i;
+
+        mesh.SetIndices(indices, MeshTopology.Points, 0);
+
+        mf.mesh = mesh;
+    }
+}
